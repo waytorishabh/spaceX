3046ac978b81f82a931e28a66d9510c3b57a8595
waytorishabh <53146725+waytorishabh@users.noreply.github.com>
2021-02-07 11:56:06 +0530

    minor

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/spaceX'));
+app.use(express.static('./dist/spaceX'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/spaceX/index.html'));
+res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'});
 });
 
 // Start the app by listening on the default Heroku port
